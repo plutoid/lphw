80e64b2f05cb7da122773ad45c9b0667fb8c5e29
Hua Tian <regal2u@gmail.com>
2014-06-09 21:00:33 -0700

    add change time

--- up.sh
+++ up.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo -n "Commit Message: ";read CI_MSG
+#Mon Jun  9 21:00:17 PDT 2014
 
+echo -n "Commit Message: ";read CI_MSG
 git add ./*
 git commit -m "$CI_MSG"
 git push origin master
