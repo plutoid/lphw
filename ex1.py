cb20e4e8d280554fc6ef44fb50a596144581097f
TianHua <regal2u@gmail.com>
2014-06-11 22:23:49 +0800

    first commit

---
+++ ex1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# coding: utf-8
+print "hello world"
+print "世界你好"
+print "hello Again"
+print "I like typing this."
+print "this is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
