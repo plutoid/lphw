92575312532ab8654b19c802a08639f52b9833d4
Hua Tian <regal2u@gmail.com>
2014-08-07 16:15:59 -0700

    ex18

--- ex16.py
+++ ex16.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python 
 
-# w3m http://learnpythonthehardway.org/book/ex15.html
+# w3m http://learnpythonthehardway.org/book/ex16.html
 
 from sys import argv
 
 script, filename = argv
 
-txt = open(filename)
+print"We 're going to erase%r." % filename
+print "If you don't want that, hit CTRL-C(^C)."
+print "If you do want that, hit RETURN."
 
-print "here'syour file %r: " % filename
-print txt.read()
+raw_input("?")
 
-print "Type the filename again:"
-file_again = raw_input("> ")
-
-txt_again = open(file_again)
-print txt_again.read()
-
+print :x
